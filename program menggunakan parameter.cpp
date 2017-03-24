f848dd1273107fa001c3d73e240c755bda1c09d1
RayvalHidayat <rayvalrayzo@gmail.com>
2017-03-24 21:09:08 +0700

    Add files via upload

---
+++ program menggunakan parameter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int tambahkan(int a, int b);
+
+int main () {
+
+
+cout << "hasil : " << tambahkan (6, 5);
+return 0;
+
+}
+
+int tambahkan (int a, int b) {
+
+return a + b;
+
+
+} 
